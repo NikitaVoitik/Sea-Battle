b656c4dc44a1e60d680b5897944caf928f222bd8
TinyColonel <69681605+TinyColonel@users.noreply.github.com>
2020-08-15 10:58:23 +0300

    Add files via upload

---
+++ ConsoleApplication1/Header.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<deque>
+#include<utility>
+#include<string>
+#include<set>
+#include<iterator>
+#include<queue>
+#include<cstring>
+#include<stack>
+#include<math.h>
+#include<algorithm>
+#include<Windows.h>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
